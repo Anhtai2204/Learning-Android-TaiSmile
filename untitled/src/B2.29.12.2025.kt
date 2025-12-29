f39d02e6f529ec1ee6c4010e824e5ad75968d726
Tai Smile <facebookketbangiaoluu@gmail.com>
2025-12-29 20:38:57 +0700

    rebase

--- untitled/src/B2.29.12.2025.kt
+++
@@ -1,6 +0,0 @@
-fun main () {
-    val ThongBao = false
-    println(
-        if (ThongBao) "Bạn đã đăng nhập"
-        else "Vui lòng đăng nhập")
-}
--- untitled/src/B4.29.12.2025.kt
+++
@@ -1,8 +0,0 @@
-fun main () {
-    val dangNhap = 0
-    val dayDangNhap = when (dangNhap) {
-        1 -> "Thông báo phù hợp"
-        else -> "Không phù hợp"
-    }
-    println(dayDangNhap)
-}
